d1c90b4bc60cf864179cb27d4876781661bb2ff0
BossHacke <vonguyenphihung123@gmail.com>
2024-10-03 15:17:00 +0700

    addComponent

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Prop from "./props";
+import AddComponent from "./add_component";
 // import FunctionComponent from "./function_component";
 
 
 class Form extends React.Component {
 
     state = {
-        firstName: '',
-        lastName: '',
         arrJobs: [
             { id: 'abcJob1', title: 'Developers', salary: '500 $' },
             { id: 'abcJob2', title: 'Testers', salary: '400 $' },
@@ -16,37 +15,45 @@ class Form extends React.Component {
     }
 
 
-    onHandleFirstName = (event) => {
-        this.setState({
-            firstName: event.target.value,
-        })
-    }
+    // onHandleFirstName = (event) => {
+    //     this.setState({
+    //         firstName: event.target.value,
+    //     })
+    // }
 
-    onHandleLastName = (event) => {
-        this.setState({
-            lastName: event.target.value,
-        })
-    }
+    // onHandleLastName = (event) => {
+    //     this.setState({
+    //         lastName: event.target.value,
+    //     })
+    // }
 
-    onHandleSubmit = (event) => {
-        event.preventDefault();
-        this.setState(this.state);
-        console.log(this.state);
+    // onHandleSubmit = (event) => {
+    //     event.preventDefault();
+    //     this.setState(this.state);
+    // }
+
+    addNewJob = (job) => {
+        this.setState(
+            {
+                arrJobs: [...this.state.arrJobs, job],
+            }
+        )
     }
 
     render() {
         return (
             <>
-                <div>
+                <AddComponent addNewJob={this.addNewJob} />
+                {/* <div>
                     Form
                 </div>
                 <form>
                     <label htmlFor="fname">First name</label><br />
-                    <input type="text" value={this.state.firstName} onChange={(event) => { this.onHandleFirstName(event) }} /><br />
+                    <input type="text" value={this.state.arrJobs.title} onChange={(event) => { this.onHandleFirstName(event) }} /><br />
                     <label htmlFor="lname">Last name</label><br />
-                    <input type="text" value={this.state.lastName} onChange={(event) => { this.onHandleLastName(event) }} /><br />
+                    <input type="text" value={this.state.arrJobs.salary} onChange={(event) => { this.onHandleLastName(event) }} /><br />
                     <input type="submit" value="Submit" onClick={(event) => { this.onHandleSubmit(event) }} />
-                </form >
+                </form > */}
                 <Prop arrJobs={this.state.arrJobs} />
                 {/* <FunctionComponent arrJobs={this.state.arrJobs} /> */}
             </>
